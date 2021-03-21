368e17095644aca2f5c9965f1fba44eac3b9c41e
Amal sankar <amalsankar29@gmail.com>
2021-03-21 14:00:47 +0530

    first commit

---
+++ userapp/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render,redirect
+from . forms import UserRegistrationForm,UserLoginForm
+from django.contrib.auth import authenticate,login,logout
+
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='login')
+def home(request):
+    return render(request,'dashboard.html')
+
+
+
+
+def Register(request):
+    context={}
+    if request.POST:
+        form=UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        context['register_form']=form 
+    else:
+        form=UserRegistrationForm()
+        context['register_form']=form 
+
+
+    return render(request,"register.html",context)
+
+
+
+def login_view(request):
+    context={}
+    if request.POST:
+        form=UserLoginForm(request.POST)
+        if form.is_valid():
+            email=request.POST['email']
+            password=request.POST['password']
+            user=authenticate(request,email=email,password=password)
+
+            if user is not None:
+                login(request,user)
+                return redirect("dashboard")
+
+        else:
+            context['login_form']=form
+
+        
+    else:
+        form=UserLoginForm()
+        context['login_form']=form
+
+    return render(request,'login.html',context)  
+
+
+
+
+def logout_view(request):
+     logout(request)
+     return redirect("login")   
